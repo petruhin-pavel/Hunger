b34a3c70a148a9dc1384660f89cd27d3a77931ea
Pavel <64465001+ilbuild@users.noreply.github.com>
2020-09-18 14:42:13 +0300

    done

--- js/script.js
+++ js/script.js
@@ -37,3 +37,12 @@ var mySwiper = new Swiper('.swiper-container', {
     });
   });
 })(jQuery);
+
+// Плавный скролл по якорям
+$(document).ready(function () {
+  $("body").on('click', '[href*="#"]', function (e) {
+    var fixed_offset = 0;
+    $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+    e.preventDefault();
+  });
+});
