a5a008e5d9b1cec0158dd0b76c76d89306ea85f5
Pavel <64465001+ilbuild@users.noreply.github.com>
2020-09-18 15:28:06 +0300

    fix burger

--- js/script.js
+++ js/script.js
@@ -1,16 +1,41 @@
 $(document).ready(function () {
+  // Бургер
   $('.burger-nav').click(function (event) {
     $('.burger-nav').toggleClass('burger-nav-active');
     $('body').toggleClass('lock');
   });
-});
 
-$(document).ready(function () {
   $('.burger-nav').click(function (event) {
     $('.mobile-nav').toggleClass('mobile-nav-active');
   });
-});
 
+  $('.mobile-nav-link').click(function (event) {
+    $('body').removeClass('lock');
+    $('.burger-nav').removeClass('burger-nav-active');
+    $('.mobile-nav').removeClass('mobile-nav-active');
+  });
+
+  // Плавный скролл по якорям
+  $("body").on('click', '[href*="#"]', function (e) {
+    var fixed_offset = 0;
+    $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+    e.preventDefault();
+  });
+
+  // Табы
+  $("ul.tabs__caption").on("click", "li:not(.active)", function () {
+    $(this)
+      .addClass("price-active")
+      .siblings()
+      .removeClass("price-active")
+      .closest("div.tabs")
+      .find("div.price-wrap")
+      .removeClass("price-active")
+      .eq($(this).index())
+      .addClass("price-active");
+  });
+});
+// Слайдер
 var mySwiper = new Swiper('.swiper-container', {
   pagination: {
     el: '.projects-pagination',
@@ -18,31 +43,4 @@ var mySwiper = new Swiper('.swiper-container', {
     bulletActiveClass: 'projects-bullet-active',
     clickable: true
   },
-});
-
-/* effect: 'fade', */
-
-(function ($) {
-  $(function () {
-    $("ul.tabs__caption").on("click", "li:not(.active)", function () {
-      $(this)
-        .addClass("price-active")
-        .siblings()
-        .removeClass("price-active")
-        .closest("div.tabs")
-        .find("div.price-wrap")
-        .removeClass("price-active")
-        .eq($(this).index())
-        .addClass("price-active");
-    });
-  });
-})(jQuery);
-
-// Плавный скролл по якорям
-$(document).ready(function () {
-  $("body").on('click', '[href*="#"]', function (e) {
-    var fixed_offset = 0;
-    $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
-    e.preventDefault();
-  });
 });
